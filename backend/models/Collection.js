39a5b4003955e7d4840a54e775d70ce251852a20
arunbharathi-parkkavamani-dev <coc.arunbharathi@gmail.com>
2025-08-20 23:09:44 +0530

    Create Collection.js

---
+++ backend/models/Collection.js
@@ -0,0 +1,32 @@
+// models/collection.js
+import Employee from './Employee.js';
+import Department from './Department.js';
+import Designation from './Designation.js';
+import LeaveType from './LeaveType.js';
+import LeavePolicy from './LeavePolicy.js';
+import Attendance from './Attendance.js';
+import SideBar from './SideBar.js';
+import TaskType from './TaskType.js';
+import Client from './Client.js';
+import DailyActivity from './DailyActivity.js';
+import ErrorLog from './ErrorLog.js';
+import ApiHitLog from './ApiHitLog.js';
+import ProjectType from './ProjectType.js';
+
+const models = {
+  Employee,
+  Department,
+  Designation,
+  LeaveType,
+  LeavePolicy,
+  Attendance,
+  SideBar,
+  TaskType,
+  Client,
+  DailyActivity,
+  ErrorLog,
+  ApiHitLog,
+  ProjectType
+};
+
+export default models;
