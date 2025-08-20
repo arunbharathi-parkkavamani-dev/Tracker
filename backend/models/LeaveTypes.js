5d7a1841caa3b6940677f1d87f2d5f2d7f75f681
arunbharathi-parkkavamani-dev <coc.arunbharathi@gmail.com>
2025-08-20 21:58:14 +0530

    Create LeaveTypes.js

---
+++ backend/models/LeaveTypes.js
@@ -0,0 +1,22 @@
+// models/LeaveType.js
+const mongoose = require('mongoose');
+
+const LeaveTypeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    unique: true
+  },
+  description: { type: String },
+  maxDaysPerMonth: { type: Number, default: 0 },
+  maxDaysPerYear: { type:Number, default: 0},
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+LeaveTypeSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model('LeaveType', LeaveTypeSchema);
