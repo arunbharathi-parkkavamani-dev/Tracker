751e2cc1f6b4ed7ad6f49dae48d263e77cc9955f
arunbharathi-parkkavamani-dev <coc.arunbharathi@gmail.com>
2025-08-21 08:57:05 +0530

    Create routes/populateRoutes.js

---
+++ backend/routes/populateRoutes.js
@@ -0,0 +1,10 @@
+// routes/populateRouter.js
+import express from "express";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { handlePopulate } from "../helpers/populateHelper.js";
+
+const router = express.Router();
+
+router.all("/:action/:model/:id?", authMiddleware, handlePopulate);
+
+export default router;
