49f0b50040f0d220b7da85153caf3ba9156b76b7
arunbharathi-parkkavamani-dev <coc.arunbharathi@gmail.com>
2025-11-25 17:47:06 +0530

    Updated Model.tsx

--- App/app/modal.tsx
+++ App/app/modal.tsx
@@ -1,7 +1,5 @@
-import "../global.css"
 import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
-
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 
